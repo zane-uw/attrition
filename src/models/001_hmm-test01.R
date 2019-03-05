0a62b4c17b225aa776e58045e97df7bd2d066b11
zane-uw <zane@uw.edu>
2019-03-04 16:29:29 -0800

    add initial modeling file

---
+++ src/models/001_hmm-test01.R
@@ -0,0 +1,9 @@
+rm(list = ls())
+gc()
+
+# load...
+
+
+# notes -------------------------------------------------------------------
+
+# Setup for modeling students' quarterly courses in sequence for HMM
